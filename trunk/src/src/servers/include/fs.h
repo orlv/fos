da9a4bca70f4f077c93f5fb9c95be3f7d60471b9
ofedorov <ofedorov@9cbd9824-e416-0410-b9a3-c693083ff851>
2007-05-24 08:05:06 +0000

    servers/include/fs.h added

---
+++ trunk/src/src/servers/include/fs.h
@@ -0,0 +1,44 @@
+/*
+  include/fs.h
+  Copyright (C) 2006 Oleg Fedorov
+*/
+
+#ifndef __FS_H
+#define __FS_H
+
+#include <types.h>
+
+struct file_info_t {
+  u32_t type;			/* тип объекта (regular file/directory/other object) */
+  u32_t nlink;			/* количество ссылок */
+
+  uid_t uid;			/* id владельца */
+  gid_t gid;			/* gid владельца */
+  mode_t mode;			/* права доступа */
+
+  size_t size;			/* размер (в байтах) */
+  u32_t atime;			/* время последнего доступа */
+  u32_t mtime;			/* время последней моификации */
+};
+
+#define FTypeObject    1
+#define FTypeDirectory 2
+
+#define MAX_PATH_LEN 4096
+#define MAX_NAME_LEN 256
+
+#define FS_CMD_ACCESS    0
+
+#define NAMER_CMD_ACCESS 0
+#define NAMER_CMD_ADD    1
+#define NAMER_CMD_REM    2
+#define NAMER_CMD_LIST   3
+
+#define PID_NAMER 2
+
+struct fs_message {
+  u32_t cmd;
+  char name[MAX_PATH_LEN];
+} __attribute__ ((packed));
+
+#endif

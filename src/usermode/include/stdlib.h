9fb7d9e4249f8f7df784494ba5909e9ec9978117
Oleg Orlov <oleg.orlv@gmail.com>
2007-07-22 17:23:58 +0000

    -moving

--- src/usermode/include/stdlib.h
+++ src/usermode/include/stdlib.h
@@ -10,4 +10,20 @@
 
 asmlinkage void exit(int status);
 
+#ifdef __cplusplus
+extern "C"
+#endif
+void *malloc(size_t size);
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void free(void *ptr);
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void *realloc(void *ptr, size_t size);
+
+
 #endif

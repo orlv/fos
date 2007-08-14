c4915ec7c820b083b443a8cdc92b5f1a2d4415fa
Oleg Orlov <oleg.orlv@gmail.com>
2007-08-14 21:28:09 +0000

    - Makefile's updated

--- include/fos/version.h
+++ include/fos/version.h
@@ -1,8 +1,8 @@
 /* 
-    include/fos/version.h 
-    Automatically generated - do not modify 
-    Copyright (C) 2004-2007 Oleg Fedorov 
-*/ 
-
+include/fos/version.h
+Automatically generated - do not modify
+Copyright (C) 2004-2007 Oleg Fedorov
+*/
+ 
 #define VERSION	"0.0.43.9"
-#define BUILD	17507
+#define BUILD	17508

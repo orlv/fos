ceffa70df03a1812f69ba4269fc187e59ed3c868
Oleg Orlov <oleg.orlv@gmail.com>
2007-10-25 16:32:07 +0000

    -upd

--- include/fos/version.h
+++ include/fos/version.h
@@ -4,5 +4,5 @@ Automatically generated - do not modify
 Copyright (C) 2004-2007 Oleg Fedorov
 */
  
-#define VERSION	"0.0.45.1"
-#define BUILD	18025
+#define VERSION	"0.0.45.2"
+#define BUILD	"Thu Oct 25 16:23:47 UTC 2007"

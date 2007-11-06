1cee9f0d394c1f2bac790c051aa51c07e8cd41f7
Oleg Orlov <oleg.orlv@gmail.com>
2007-11-06 07:23:14 +0000

    test updated

--- src/usermode/test/main.c
+++ src/usermode/test/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "gui.h"
 asmlinkage int main(int argc, char ** argv)
 {
+	int i;
 	GUIInit();
-	int winhandle = CreateWindow(0, 10, 10, 400, 50, "4 окна друг на друге :)", WC_WINDOW);
+	char title[128];
+	strcpy(title, "Окно ");
+	if(argc>1)
+		for(i=1; i<argc; i++)
+			strcat(title, argv[i]);
+
+	int winhandle = CreateWindow(0, 10, 10, 400, 50, title, WC_WINDOW);
 	int class, handle, a0, a1, a2, a3;
 	while(1) {
 		WaitEvent(&class, &handle, &a0, &a1, &a2, &a3);

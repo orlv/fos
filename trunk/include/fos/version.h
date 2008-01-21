e2cdee1827b445323ce74347f5b7d1b01152b1b7
ofedorov <ofedorov@9cbd9824-e416-0410-b9a3-c693083ff851>
2008-01-21 00:25:27 +0000

    +template index

--- trunk/include/fos/version.h
+++ trunk/include/fos/version.h
@@ -4,5 +4,5 @@ Automatically generated - do not modify
 Copyright (C) 2004-2007 Oleg Fedorov
 */
  
-#define VERSION	"0.0.45.6"
-#define BUILD	"20080120"
+#define VERSION	"0.0.45.7"
+#define BUILD	"20080121"
